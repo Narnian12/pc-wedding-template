cd7c47b2e2f102685f7120630fcdb2012a4c2252
Crystal <crysun@umich.edu>
2022-05-11 20:35:48 -0400

    Blue App Bar
    - Links to each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
-      <Link to="/event">Event</Link>
-      <Link to="/gallery">Gallery</Link>
-      <Link to="/home">Home</Link>
-      <Link to="/rsvp">RSVP</Link>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          <Link to="/event">Event</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/home">Home</Link>
+          <Link to="/rsvp">RSVP</Link>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </>
   );
 }
