fe1cb7c23f9dd11b8d19494fecd8874cec3fb3fe
Peter S <pitaah@umich.edu>
2022-05-11 20:09:28 -0400

    - Cleanup

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Event() {
   return <div>Event page</div>;
 }
--- src/pages/Rsvp.tsx
+++ src/pages/Rsvp.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Rsvp() {
   return <div>Rsvp page</div>;
 }
